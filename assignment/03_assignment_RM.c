4ca5e0f863cf0d5f9f4bb5b59e46a79030edb482
Subhranil Sarkar <subhranil2605@gmail.com>
2022-01-20 19:25:04 +0530

    hfg

---
+++ assignment/03_assignment_RM.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+typedef struct node* node;
+
+/* ------------------- Function declarations --------------- */
+node displayList(node p);
+node createNode();
+node insertBeg(node head);
+node createList();
+
+/* ------------------- Function definitions --------------- */
+
+// displaying
+node displayList(node p) {
+    while (1) {
+        if (p != NULL) {
+            printf("%d-->", p -> data);
+        } else {
+            printf("NULL");
+            break;
+        }
+    }
+}
+
+
+// create a single node
+node createNode() {
+    node temp;
+    temp = (node) malloc(sizeof(node));
+    temp -> next = NULL;
+    return temp;
+}
+
+// inserting from the beginning
+node insertBeg(node head) {
+    node newNode;
+    int value;
+
+    newNode = createNode();
+    printf("\nEnter the new value >> ");
+    scanf("%d", &value);
+    newNode -> data = value;
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        newNode -> next = head;
+        head = newNode;
+    }
+}
+
+// creating a list
+node createList() {
+    node head;
+    char ch;
+
+    head = NULL;
+
+    while (ch != 'q') {
+        head = insertBeg(head);
+        printf("\nPress 'q' to stop OR Press any key to continue >> ");
+        scanf("\n%c", &ch);
+    }
+    return head;
+}
+
+
+
+// main function
+void main() {
+    node list;
+    list = createList();
+    displayList(list);
+}
---
+++ sorting algos/bubble_sort.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+    int numbers[] = {10, 2, 45, 11, 78};
+    int i;
+    bool swap;
+    
+    for (i=0; i < 5; i++) {
+        printf("%d\t", numbers[i]);
+    }
+}
