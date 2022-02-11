7356e0a95c3d8bee8cbfbdb79417ffc0c9de3efb
Subhranil Sarkar <subhranil2605@gmail.com>
2022-02-12 00:28:43 +0530

    polynomial/03_addition:

--- polynomial/03_addition.c
+++ polynomial/03_addition.c
@@ -3,7 +3,7 @@
 
 
 struct node {
-    float coef;
+    int coef;
     int exp;
     struct node* next;
 };
@@ -24,7 +24,7 @@ node createNode() {
 // insert from the beginning
 node insertBeg(node head) {
     node newNode;
-    float value;
+    int value;
     int exp;
 
     newNode = createNode();
@@ -33,7 +33,7 @@ node insertBeg(node head) {
     scanf("%d", &exp);
 
     printf("\nEnter its coefficient >> ");
-    scanf("%f", &value);
+    scanf("%d", &value);
 
     newNode -> coef = value;
     newNode -> exp = exp;
@@ -68,7 +68,7 @@ node createPoly() {
 void displayPoly(node p) {
     while (1) {
         if (p != NULL) {
-            printf("%0.2f x^%d --> ", p -> coef, p -> exp);
+            printf("%d x^%d --> ", p -> coef, p -> exp);
         } else {
             printf("NULL\n");
             break;
@@ -77,8 +77,85 @@ void displayPoly(node p) {
     }
 }
 
+// appending function
+node append(node head, int coef, int exp) {
+    node newNode, ptr;
+
+    if (head == NULL) {
+        newNode = createNode();
+        newNode -> coef = coef;
+        newNode -> exp = exp;
+        newNode -> next = NULL;
+        head = newNode;
+    } else {
+        ptr = head;
+        while (ptr -> next != NULL) {
+            ptr = ptr -> next;
+        }
+        newNode = createNode();
+        newNode -> coef = coef;
+        newNode -> exp = exp;
+        newNode -> next = NULL;
+        ptr -> next = newNode;
+    }
+    return head;
+}
+
+
+// polynomial addition
+node polyAdd(node head1, node head2, node head3) {
+    node p, q;
+    int sumNum;
+    p = head1;
+    q = head2;
+
+    while (p != NULL && q != NULL) {
+        if (p -> exp == q -> exp) {
+            sumNum = p -> coef + q -> coef;
+            head3 = append(head3, sumNum, p -> exp);
+            p = p -> next;
+            q = q -> next;
+        } else if (p -> exp < q -> exp) {
+            head3 = append(head3, q -> coef, q -> exp);
+            q = q -> next;
+        } else if (p -> exp > q -> exp) {
+            head3 = append(head3, p -> coef, p -> exp);
+            p = p -> next;
+        }
+    }
+
+    if(q == NULL){
+        while (p != NULL) {
+            head3 = append(head3, p -> coef, p -> exp);
+            p = p -> next;
+        }
+    }
+
+    if (p == NULL){
+        while (q != NULL) {
+            head3 = append(head3, q -> coef, q -> exp);
+            q = q -> next;
+        }
+    }
+}
+
+
+
 void main() {
-    node p = createPoly();
-    printf("The polynomial is =>\t");
-    displayPoly(p);
+    int coef, exp;
+    node p1, p2, p3 = NULL;
+
+    p1 = createPoly();
+    printf("\n-------------Enter the second polynomial-----------\n");
+    p2 = createPoly();
+
+    printf("\nThe First polynomial is =>\t");
+    displayPoly(p1);
+    printf("\nThe Second polynomial is =>\t");
+    displayPoly(p2);
+
+    p3 = polyAdd(p1, p2, p3);
+    printf("\nThe Result polynomial is =>\t");
+    displayPoly(p3);
+    
 }
