b1b5eda1ac035edb3b03bf4667c4aeecfed98bc8
Subhranil Sarkar <subhranil2605@gmail.com>
2022-03-21 00:21:42 +0530

    search/binary_searh:

--- search/binary_searh.c
+++ search/binary_searh.c
@@ -1,26 +1,47 @@
 #include<stdio.h>
 
 
-
-int binarySearch(int arr[], int left, int right, int search) {
+// binary search iterative function
+int binarySearchIterative(int arr[], int left, int right, int search) {
     int middle;
     while (left <= right) {
-        middle = l + (r - l) / 2;
+        middle = left + (right - left) / 2;
 
-        if (arr[middle] = search) {
+        if (arr[middle] == search) {
             return middle;
         } else if (arr[middle] < search) {
-            l = middle + 1;
+            left = middle + 1;
         } else {
-            r = middle - 1;
+            right = middle - 1;
         }
     }
     return -1;
 }
 
 
+// binary search recursive function
+int binarySearchRecursive(int arr[], int left, int right, int search) {
+    int middle;
+    while (left <= right) {
+        middle = left + (right - left) / 2;
+
+        if (arr[middle] == search) {
+            return middle;
+        } else if (arr[middle] < search) {
+            // the element is in the right part
+            return binarySearchRecursive(arr, middle + 1, right, search);
+        } else {
+            // the element is in left part
+            return binarySearchRecursive(arr, left, middle - 1, search);
+        }
+    }
+    return -1;
+}
+
+
+// main function
 void main() {
-    int i, n;
+    int i, n, search, pos, choice;
 
     printf("Enter number of elements in the array: ");
     scanf("%d", &n);
@@ -29,11 +50,38 @@ void main() {
 
     printf("\nEnter the %d elements in sorted order\n", n);
     for (i = 0; i < n; i++) {
-        printf("%d-th element is: ");
+        printf("%d-th element is: ", i);
         scanf("%d", &a[i]);
     }
 
     printf("\nEnter the element to be searched: ");
     scanf("%d", &search);
+
+
+    // Menu
+    printf("\n====== Binary Search Type Choose Menu =======\n");
+    printf("Press 1: Using Iterative function\n");
+    printf("Press 2: Using Recursive function\n");
+    printf("\n=============\n");
+    printf("Enter your choice >> ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            pos = binarySearchIterative(a, 0, n-1, search);
+            break;
+        case 2:
+            pos = binarySearchRecursive(a, 0, n-1, search);
+            break;
+        
+        default:
+            printf("\nIncorrect choice!!!\n");
+            break;
+    }
+
+
+    (pos != -1) 
+    ? printf("The element %d is found at %d\n", search, pos) 
+    : printf("The element %d is not found!\n", search);
     
 } 
