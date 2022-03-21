948ce9c66ddc9945da38ad280c35fdc0543d1baa
Subhranil Sarkar <subhranil2605@gmail.com>
2022-03-21 23:05:23 +0530

    "sorting algos/bubble_sort.c":

--- sorting algos/bubble_sort.c
+++ sorting algos/bubble_sort.c
@@ -1,11 +1,68 @@
+/*
+Bubble Sorting
+*/
+
+
 #include<stdio.h>
 
-void main() {
-    int numbers[] = {10, 2, 45, 11, 78};
+// swap function
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// display elements
+void displayArray(int arr[], int n) {
     int i;
-    bool swap;
-    
-    for (i=0; i < 5; i++) {
-        printf("%d\t", numbers[i]);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
+    printf("\n");
+}
+
+
+// bubble sort
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    int swapped = 0; // 0 -> false & 1 -> true
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = 1; 
+            }
+        }
+
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+
+// main function
+void main() {
+    int i, j, n, swapped = 0;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter %d elements of the array\n", n);
+    
+    for (i = 0; i < n; i++) {
+        printf("Enter %d-th element: ", i);
+        scanf("%d", &a[i]);
+    }
+
+
+    printf("Bubble Sort");
+
+    bubbleSort(a, n);
+    displayArray(a, n);
+
+
 }
