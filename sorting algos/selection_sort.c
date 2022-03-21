d9c5863557c2dd702d7652af2b265cf76da0ec93
Subhranil Sarkar <subhranil2605@gmail.com>
2022-03-22 00:18:19 +0530

    "sorting algos/selection_sort.c":

--- sorting algos/selection_sort.c
+++ sorting algos/selection_sort.c
@@ -29,7 +29,7 @@ void selectionSort(int arr[], int n) {
     int swapped = 0; // 0 -> false & 1 -> true
     for (i = 0; i < n - 1; i++) {
         minIndex = i;
-        for (j = i + 1; j < n - i; j++) {
+        for (j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
