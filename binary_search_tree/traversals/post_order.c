37fe5db3ca9be394c1e94ec2764986888ec49a67
Subhranil Sarkar <subhranil2605@gmail.com>
2022-03-22 20:11:10 +0530

    binary_search_tree/traversals:

---
+++ binary_search_tree/traversals/post_order.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+typedef struct node* node;
+
+
+// creating a single node
+node createNode(int val) {
+    node temp;
+    temp = (node) malloc(sizeof(node));
+    temp -> data = val;
+    temp -> left = temp -> right = NULL;
+    return temp;
+}
+
+
+// inserting a node in the tree
+node insert(node tree, int val) {
+    if (tree == NULL) {
+        tree = createNode(val);
+    } else {
+        // if the value is lesser than the root node, then it will be inserted in left
+        if (val < tree -> data) {
+            tree -> left = insert(tree -> left, val);
+        } 
+        // if the value is greater than the root node, then it will be inserted in right
+        else {
+            tree -> right = insert(tree -> right, val);
+        }
+    }
+    return tree;
+}
+
+
+// postorder traversal
+void postorder(node tree) {
+    if (tree != NULL) {
+        postorder(tree -> left);
+        postorder(tree -> right);
+        printf("%d -> ", tree -> data);
+    }
+}
+
+
+
+
+// main function
+void main() {
+    node tree; 
+    
+    tree = NULL;
+
+    tree = insert(tree, 5);
+    tree = insert(tree, 6);
+    tree = insert(tree, 4);
+
+    postorder(tree);
+}
