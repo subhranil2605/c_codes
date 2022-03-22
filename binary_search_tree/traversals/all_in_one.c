f7b1229416168da659ed505b55778f6242a02046
Subhranil Sarkar <subhranil2605@gmail.com>
2022-03-22 20:13:36 +0530

    binary_search_tree/traversals/all_in_one: create

---
+++ binary_search_tree/traversals/all_in_one.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+typedef struct node* node;
+
+
+// creating a single node
+node createNode(int val) {
+    node temp;
+    temp = (node) malloc(sizeof(node));
+    temp -> data = val;
+    temp -> left = temp -> right = NULL;
+    return temp;
+}
+
+
+// inserting a node in the tree
+node insert(node tree, int val) {
+    if (tree == NULL) {
+        tree = createNode(val);
+    } else {
+        // if the value is lesser than the root node, then it will be inserted in left
+        if (val < tree -> data) {
+            tree -> left = insert(tree -> left, val);
+        } 
+        // if the value is greater than the root node, then it will be inserted in right
+        else {
+            tree -> right = insert(tree -> right, val);
+        }
+    }
+    return tree;
+}
+
+
+// preorder traversal
+void preorder(node tree) {
+    if (tree != NULL) {
+        printf("%d -> ", tree -> data);
+        preorder(tree -> left);
+        preorder(tree -> right);
+    }
+}
+
+
+// in-order traversal
+void inorder(node tree) {
+    if (tree != NULL) {
+        inorder(tree -> left);
+        printf("%d -> ", tree -> data);
+        inorder(tree -> right);
+    }
+}
+
+
+// postorder traversal
+void postorder(node tree) {
+    if (tree != NULL) {
+        postorder(tree -> left);
+        postorder(tree -> right);
+        printf("%d -> ", tree -> data);
+    }
+}
+
+
+
+
+// main function
+void main() {
+    node tree; 
+    
+    tree = NULL;
+
+    tree = insert(tree, 5);
+    tree = insert(tree, 6);
+    tree = insert(tree, 4);
+
+    printf("\nPreorder Traversal: ");
+    preorder(tree);
+
+    printf("\nInorder Traversal: ");
+    inorder(tree);
+
+    printf("\nPostorder Traversal: ");
+    postorder(tree);
+}
